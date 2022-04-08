8e033774e5d6140b0035076e79866e757c93ab3d
Saulo M. Amaral <saulomas@gmail.com>
2022-04-08 20:25:35 -0300

    Conclusão do 2º projeto

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,20 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome...</p>
-  <teleport to="#modals" v-if="showModal">
-    <Modal theme="" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">Sign up now</a>
-        <a href="#">More info</a>
-      </template>
-      <h1>Ninja Givaway</h1>
-      <p>Grab your ninja swag for half price!</p>
-    </Modal>
-  </teleport>
-  
-  <teleport to="#modals" v-if="showModalTwo">
-    <Modal @close="toggleModalTwo">
-      <h1>Sign up to the newsletter</h1>
-      <p>For updates and promo codes!</p>
-    </Modal>
-  </teleport>
-
-  <button @click.alt="toggleModal">Open Modal (Alt)</button>
-  <button @click="toggleModalTwo">Open Modal</button>
+  <SignupForm></SignupForm>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import SignupForm from './components/SignupForm.vue'
 
 export default {
   name: 'App',
-  components: { Modal },
-  data() {
-    return {
-      title: 'My First Vue App :)',
-      showModal: false,
-      showModalTwo: false
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo
-    }
+  components: { 
+    SignupForm
   }
 }
 </script>
 
 <style>
-#app, #modals {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -56,9 +22,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
